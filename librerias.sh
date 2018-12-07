806cda22c3b4c0f23a8f587c4fc5aed16c7bb7ab
procamora <pablojoserocamora@gmail.com>
2018-12-07 21:40:28 +0100

    actualizado el formato UM del tfg

---
+++ librerias.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+dnf -y install texlive-matlab-prettifier texlive-apacite texlive-breakcites texlive-xpatch \
+texlive-multirow texlive-tikzpagenodes texlive-tikzmark texlive-pgfplots texlive-glossaries-spanish\
+texlive-anyfontsize texlive-framed texlive-mdframed texlive-todonotes texlive-glossaries 
